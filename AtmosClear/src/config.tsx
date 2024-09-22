60d8d0025933a75872818c9f70b676013fd26668
Jed556 <jguiriba11@gmail.com>
2024-09-23 04:23:09 +0800

    fix: revert to original

--- AtmosClear/src/config.tsx
+++ AtmosClear/src/config.tsx
@@ -1,7 +1,7 @@
 import { Server } from "./schemas/Server";
 
 export const server: Server = {
-    url: process.env.API_URL || "localhost",
-    suffix: process.env.API_SUFFIX || "",
-    port: process.env.API_PORT ? parseInt(process.env.API_PORT) : 80
+    url: "localhost",
+    suffix: "atmosclear",
+    port: 80
 }
